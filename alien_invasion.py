0cce357008d08a1b72580b348e72f47837b1adbc
spencer-DOS <dspencer.ds85@gmail.com>
2020-03-29 02:24:01 -0400

    Ship moves right 1 pixel now.

--- alien_invasion.py
+++ alien_invasion.py
@@ -31,12 +31,39 @@ class AlienInvasion:
         while True:
             self._check_events()
             self._update_screen()
-            # Watch for keyboard and mouse events.
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
 
-            # Redraw the screen during each pass through the loop.
+    def _check_events(self):
+        """Respond to keypresses and mouse events."""
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RIGHT:
+                    # Move the ship to the right.
+                    self.ship.rect.x += 1
+                self._check_keydown_events(event)
+            elif event.type == pygame.KEYUP:
+                self._check_keyup_events(event)
+
+    def _check_keydown_events(self, event):
+        """Respond to keypresses."""
+        if event.type == pygame.K_RIGHT
+            # Move the ship to the right.
+            self.ship.moving_right = True
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = True
+        elif event.key == pygame.K_q:
+            sys.exit()
+
+    def _check_keyup_events( self, event ):
+        """Respond to key releases."""
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = False
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = False
+
+    def _update_screen(self):
+            """Update images on the screen, and flip to the new screen."""
             self.screen.fill(self.settings.bg_color)
             # Draw ship on top of screen background.
             self.ship.blitme()
